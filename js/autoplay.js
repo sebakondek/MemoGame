43db0b155db9fa53f5ee16d13bfe4dedd6eea711
Sebastian Kondek <sebastian.kondek@globant.com>
2017-06-19 17:23:46 -0300

    correciones

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,51 +1,54 @@
-function autoplay(qty, cards){
+function autoplay(pairs, cards){
 	var autoplayCards = [];
-	var intervalID;
+	var intervalID = null;
 	var i = 0;
+	var click = 0;
 	play();
-	
-	function loop(click) {
-		alert(click);
-		$("#square" + i).click();
-		autoplayCards.push({id: "square" + i, value: getCardValue("square" + i, cards)});
 
-		if(autoplayCards.length > 1){
-			hasMatch();
+	function play(){
+		intervalID = setInterval(function(){
+			if(i < pairs * 2){
+				clickCard();
+				i++;
+			} else {
+				clearInterval(intervalID);
+				intervalID = null;
+			}
+		}, 1200);
+	}
+
+	if(intervalID)
+		return;
+	
+	function clickCard() {
+		var square = $("#square" + i).click();
+		var squareID = square.attr("id");
+
+		click++;
+
+		autoplayCards.push({id: squareID, value: getCardValue(squareID, cards), flipped: false});
+
+		var match = hasMatch();
+
+		if(!(isNaN(match))){
+			if(click % 2 === 1){
+				click++;
+				$("#square" + match).click();
+				autoplayCards[match].flipped = true;
+				autoplayCards[(autoplayCards.length - 1)].flipped = true;
+			}
+			// else{
+			// 	i--;
+			// }
 		}
 	}
 
 	function hasMatch(){
-		var match = false;
-
-		for(var j = 0; j < autoplayCards.length - 1 && !match; j++){
-
-			if((autoplayCards[(autoplayCards.length - 1)].value === autoplayCards[j].value)){
-				$("#square" + j).click();
-				match = true;
-				return true;
+		for(var j = 0; j < autoplayCards.length - 1; j++){
+			if(!(autoplayCards[j].flipped) && (autoplayCards[(autoplayCards.length - 1)].value === autoplayCards[j].value)){
+				return j;
 			}
 		}
-
-		return false;
-	}
-
-	
-	if(intervalID)
-		return;
-	
-	function play(){
-		var click = 0;
-		intervalID = setInterval(function(){
-			if(i < qty * 2){
-				click = click % 2;
-				loop(click);
-				i++;
-			}
-			else{
-				clearInterval(intervalID);
-				intervalID = null;
-			}
-		}, 1500);
 	}
 }
 
--- js/game.js
+++ js/game.js
@@ -1,135 +1,135 @@
 $(document).ready(function(){
-(function(){
-var flipped;
-var elems;
-var cards;
-var points;
+	(function(){
+		var flipped;
+		var elems;
+		var cards;
+		var points;
 
-$("#start").on("click",function(){
-		start();
-	})
+		$("#start").on("click",function(){
+				start();
+			})
 
-function start(){
-	$("#wrapper").empty();
-	$("#autoplay").show();
-	
-	points = 0;
-	flipped = 0;
-	elems = [];
-	cards = [];	
-	
-	do{
-		var qty = Math.floor(prompt("How many pair of numbers would you like to play with? (2-10)"));
-	}while((qty < 2 || qty > 10))
-	
-	$("#start").text("RESTART");
-	
-	grid(qty);
-	assignValues(qty);
-	
-	$("#autoplay").on("click", function(){
-		autoplay(qty, cards);
-	})
-	
-	$(".square").on("click",function(){
-		flip($(this).attr("id"));
-	})
-}
-
-
-function assignValues(qty){
-	var numbers=[];
-	
-	for(var i = 0; i < 2; i++){
-		for(var j = 0; j < qty; j++){
-			numbers.push(j);
-		}
-	}
-	
-	var length = numbers.length;
-	
-	for(var i = 0; i < length; i++){
-		var pos = Math.floor(Math.random() * numbers.length);
-		cards.push({square: "square" + i, value: numbers[pos]});
-		numbers.move(pos, (numbers.length - 1));
-		numbers.pop();
-	}
-}
-
-
-Array.prototype.move = function (old_index, new_index) {
-    if (new_index >= this.length) {
-        var k = new_index - this.length;
-        while ((k--) + 1) {
-            this.push(undefined);
-        }
-    }
-    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-}
-
-
-function flip(id){
-	var elem = $("#" + id );
-	
-	if(flipped < 2){
-		var cardId = getCardValue(id, cards);
-		
-		if(!elem.hasClass("flip")) {
-			elem.removeClass("flip-over");
-			elem.addClass("flip");
-			elem.children(".back").addClass("n" + cardId).append('<p class="number">' + cardId + '</p>');
-			flipped++;
-			elems.push(elem);
-		}
-	}
-	
-	if(flipped == 2){
-		setTimeout(function(){
-			if(!checkEquals(elems)){
-				for(var i = 0; i < 2; i++){
-					elems[i].removeClass("flip");
-					elems[i].addClass("flip-over");
-				}
-				
-				setTimeout(function(){
-					for(var i = 0; i < 2; i++){
-						elems[i].children(".back").empty();
-						elems[i].children(".back").removeClass("n" + getCardValue(elems[i].attr("id"), cards));
-					}
-					
-					elems = [];
-					flipped = 0;
-				}, 250);
-			} else {
-				elems = [];
-				flipped = 0;
-			}			
-					
-			if(points === (cards.length / 2))
-				if(confirm("Congratulations! Would you like to play again?"))
-					start();
+		function start(){
+			$("#wrapper").empty();
+			$("#autoplay").show();
 			
-		}, 900);
-	}
-}
-
-
-function checkEquals(elems){
-	var values = [];
-	
-	for(var i = 0, j = 0; i < cards.length && j < 2; i++){
-		if(elems[j].attr("id") === cards[i].square){
-			values.push(cards[i].value);
-			j++;
-			i = -1;
+			points = 0;
+			flipped = 0;
+			elems = [];
+			cards = [];	
+			
+			do{
+				var qty = Math.floor(prompt("How many pair of numbers would you like to play with? (2-10)"));
+			}while((qty < 2 || qty > 10))
+			
+			$("#start").text("RESTART");
+			
+			grid(qty);
+			assignValues(qty);
+			
+			$("#autoplay").on("click", function(){
+				autoplay(qty, cards);
+			})
+			
+			$(".square").on("click",function(){
+				flip($(this).attr("id"));
+			})
 		}
-	}
-		
-	if(values[0] === values[1]){
-		points++;
-		return true;
-	}
-	return false;
-}
-})();
+
+
+		function assignValues(qty){
+			var numbers=[];
+			
+			for(var i = 0; i < 2; i++){
+				for(var j = 0; j < qty; j++){
+					numbers.push(j);
+				}
+			}
+			
+			var length = numbers.length;
+			
+			for(var i = 0; i < length; i++){
+				var pos = Math.floor(Math.random() * numbers.length);
+				cards.push({square: "square" + i, value: numbers[pos]});
+				numbers.move(pos, (numbers.length - 1));
+				numbers.pop();
+			}
+		}
+
+
+		Array.prototype.move = function (old_index, new_index) {
+		    if (new_index >= this.length) {
+		        var k = new_index - this.length;
+		        while ((k--) + 1) {
+		            this.push(undefined);
+		        }
+		    }
+		    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+		}
+
+
+		function flip(id){
+			var elem = $("#" + id );
+			
+			if(flipped < 2){
+				var cardId = getCardValue(id, cards);
+				
+				if(!elem.hasClass("flip")) {
+					elem.removeClass("flip-over");
+					elem.addClass("flip");
+					elem.children(".back").addClass("n" + cardId).append('<p class="number">' + cardId + '</p>');
+					elems.push(elem);
+					flipped++;
+				}
+			}
+			
+			if(flipped == 2){
+				setTimeout(function(){
+					if(!checkEquals(elems)){
+						for(var i = 0; i < 2; i++){
+							elems[i].removeClass("flip");
+							elems[i].addClass("flip-over");
+						}
+						
+						setTimeout(function(){
+							for(var i = 0; i < 2; i++){
+								elems[i].children(".back").empty();
+								elems[i].children(".back").removeClass("n" + getCardValue(elems[i].attr("id"), cards));
+							}
+							
+							elems = [];
+							flipped = 0;
+						}, 250);
+					} else {
+						elems = [];
+						flipped = 0;
+					}			
+							
+					if(points === (cards.length / 2))
+						if(confirm("Congratulations! Would you like to play again?"))
+							start();
+					
+				}, 900);
+			}
+		}
+
+
+		function checkEquals(elems){
+			var values = [];
+			
+			for(var i = 0, j = 0; i < cards.length && j < 2; i++){
+				if(elems[j].attr("id") === cards[i].square){
+					values.push(cards[i].value);
+					j++;
+					i = -1;
+				}
+			}
+				
+			if(values[0] === values[1]){
+				points++;
+				return true;
+			}
+			return false;
+		}
+	})();
 });
