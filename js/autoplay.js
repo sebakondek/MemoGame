7e5a7e4fda090cbcc9adf2a5f4842dfd25311c36
Sebastian Kondek <sebastian.kondek@globant.com>
2017-06-22 15:22:25 -0300

    solucionado problema de autoplay

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,52 +1,59 @@
+var autoplaying;
+
 function autoplay(pairs, cards){
+	if(autoplaying){
+		alert("It is already playing!");
+		return;
+	}
+	
 	var autoplayCards = [];
-	var intervalID = null;
 	var i = 0;
 	var click = 0;
-	play();
 
-	function play(){
-		intervalID = setInterval(function(){
-			if(i < pairs * 2){
-				clickCard();
-				i++;
-			} else {
-				clearInterval(intervalID);
-				intervalID = null;
-			}
-		}, 1200);
-	}
+	var intervalID = setInterval(function(){
+		if(i < (pairs * 2)){
+			autoplaying = true;
+			clickCard();
+			i++;
+		} else {
+			autoplaying = false;
+			clearInterval(intervalID);
+			intervalID = null;
+		}
+	}, 1400);
 
-	if(intervalID)
-		return;
+	
 	
 	function clickCard() {
 		var square = $("#square" + i).click();
 		var squareID = square.attr("id");
+		var cardValue = getCardValue(squareID, cards);
 
 		click++;
 
-		autoplayCards.push({id: squareID, value: getCardValue(squareID, cards), flipped: false});
+		console.log("1. click: " + click + ", i: " + i + ", value: " + cardValue + ", cards: " + JSON.stringify(autoplayCards));
 
-		var match = hasMatch();
+		var match = hasMatch(cardValue);
 
-		if(!(isNaN(match))){
+		if(typeof(match) === 'object'){
 			if(click % 2 === 1){
 				click++;
-				$("#square" + match).click();
-				autoplayCards[match].flipped = true;
-				autoplayCards[(autoplayCards.length - 1)].flipped = true;
+				$("#" + match.id).click();
+				autoplayCards.move(match.pos, (autoplayCards.length - 1));
+				autoplayCards.pop();
+			}else if((match.id.substring(6)) < (i - 1)){
+				i--;
 			}
-			// else{
-			// 	i--;
-			// }
+		} else {
+			autoplayCards.push({id: squareID, value: cardValue});
 		}
+		console.log("2. click: " + click + ", i: " + i + ", value: " + cardValue + ", match: " + JSON.stringify(match));
 	}
 
-	function hasMatch(){
-		for(var j = 0; j < autoplayCards.length - 1; j++){
-			if(!(autoplayCards[j].flipped) && (autoplayCards[(autoplayCards.length - 1)].value === autoplayCards[j].value)){
-				return j;
+	function hasMatch(cardValue){
+		for(var j = 0; j < autoplayCards.length; j++){
+			if(!(autoplayCards[j].flipped) && (cardValue === autoplayCards[j].value)){
+				return {id: autoplayCards[j].id, pos: j};
 			}
 		}
 	}
@@ -57,4 +64,14 @@ function getCardValue(id, cards){
 		if(cards[i].square === id)
 			return cards[i].value;
 	}
+}
+
+Array.prototype.move = function (old_index, new_index) {
+    if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
 }
